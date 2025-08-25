86a564f78d7d9f36d2bfbcaa4a6fc7635637c174
mansii1 <mansimangukiya0@gmail.com>
2025-08-25 22:11:03 +0530

    Completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
 import "./style.css";
+
+const jokeEl = document.getElementById("joke") as HTMLDivElement;
+const jokeBtn = document.getElementById("jokeBtn") as HTMLButtonElement;
+
+getJoke();
+
+// function getJoke() {
+//   fetch("https://icanhazdadjoke.com", {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((data) => (jokeEl.innerHTML = data?.joke))
+//     .catch((error) => console.log(error));
+// }.
+
+async function getJoke() {
+  const res = await fetch("https://icanhazdadjoke.com", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+jokeBtn.addEventListener("click", getJoke);
